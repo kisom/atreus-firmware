0de594b7853d83af9697211f355dba1b265990f2
Kyle Isom <kyle@cloudflare.com>
2016-04-26 18:37:43 -0700

    Clean up layer definitions.

--- layer2.h
+++ layer2.h
@@ -2,8 +2,23 @@
 // this is shared among several layouts.
 
 int layer2[44] = {
-    KEY_INSERT, KEY_HOME, KEY_UP, KEY_END, KEY_PAGE_UP, KEY_0, KEY_UP, KEY_F7, KEY_F8, KEY_F9, KEY_F10,
-    KEY_DELETE, KEY_LEFT, KEY_DOWN, KEY_RIGHT, KEY_PAGE_DOWN, KEY_0, KEY_DOWN, KEY_F4, KEY_F5, KEY_F6, KEY_F11,
-    0, 0, 0, 0, 0, KEYBOARD_LEFT_ALT, 0, KEY_F1, KEY_F2, KEY_F3, KEY_F12,
-    0, KEY_TAB, KEYBOARD_LEFT_GUI, KEYBOARD_LEFT_SHIFT, KEY_BACKSPACE, KEYBOARD_RIGHT_CTRL, KEY_SPACE, PRE_FUNCTION(1), 0, FUNCTION(0), KEY_ENTER
+    /* Layer 2, row 0, left side. */
+    KEY_INSERT, KEY_HOME, KEY_UP, KEY_END, KEY_PAGE_UP, KEY_0,
+    /* Layer 2, row 0, right side. */
+    KEY_UP, KEY_F7, KEY_F8, KEY_F9, KEY_F10,
+
+    /* Layer 2, row 1, left side. */
+    KEY_DELETE, KEY_LEFT, KEY_DOWN, KEY_RIGHT, KEY_PAGE_DOWN, KEY_0,
+    /* Layer 2, row 1, right side. */
+    KEY_DOWN, KEY_F4, KEY_F5, KEY_F6, KEY_F11,
+
+    /* Layer 2, row 2, left side. */
+    0, 0, 0, 0, 0, KEYBOARD_LEFT_ALT,
+    /* Layer 2, row 2, right side. */
+    0, KEY_F1, KEY_F2, KEY_F3, KEY_F12,
+
+    /* Layer 2, row 3, left side. */
+    0, KEY_TAB, KEYBOARD_LEFT_GUI, KEYBOARD_LEFT_SHIFT, KEY_BACKSPACE, KEYBOARD_RIGHT_CTRL,
+    /* Layer 2, row 3, right side. */
+    KEY_SPACE, PRE_FUNCTION(1), 0, FUNCTION(0), KEY_ENTER
 };
--- layout_qwerty.h
+++ layout_qwerty.h
@@ -1,14 +1,51 @@
+/*
+ * Note: the right thumb switch is defined in row 2; the left thumb
+ * switch is defined in row 3.
+ */
+
 int layer0[44] = {
-    KEY_Q, KEY_W, KEY_E, KEY_R, KEY_T, KEY_0, KEY_Y, KEY_U, KEY_I, KEY_O, KEY_P, 
-    KEY_A, KEY_S, KEY_D, KEY_F, KEY_G, KEY_0, KEY_H, KEY_J, KEY_K, KEY_L, KEY_SEMICOLON, 
-    KEY_Z, KEY_X, KEY_C, KEY_V, KEY_B, KEYBOARD_LEFT_ALT, KEY_N, KEY_M, KEY_COMMA, KEY_PERIOD, KEY_SLASH, 
-    KEY_ESC, KEY_TAB, KEYBOARD_LEFT_GUI, KEYBOARD_LEFT_SHIFT, KEY_BACKSPACE, KEYBOARD_RIGHT_CTRL, KEY_SPACE, PRE_FUNCTION(1), KEY_MINUS, KEY_QUOTE, KEY_ENTER};
+    /* Layer 0, row 0, left side. */
+    KEY_Q, KEY_W, KEY_E, KEY_R, KEY_T, KEY_0,
+    /* Layer 0, row 0, right side. */
+    KEY_Y, KEY_U, KEY_I, KEY_O, KEY_P, 
+
+    /* Layer 0, row 1, left side. */
+    KEY_A, KEY_S, KEY_D, KEY_F, KEY_G, KEY_0,
+    /* Layer 0, row 1, right side. */
+    KEY_H, KEY_J, KEY_K, KEY_L, KEY_SEMICOLON,
+
+    /* Layer 0, row 2, left side. */
+    KEY_Z, KEY_X, KEY_C, KEY_V, KEY_B, KEYBOARD_LEFT_ALT, 
+    /* Layer 0, row 2, right side. */
+    KEY_N, KEY_M, KEY_COMMA, KEY_PERIOD, KEY_SLASH, 
+
+    /* Layer 0, row 3, left side. */
+    KEY_ESC, KEY_TAB, KEYBOARD_LEFT_GUI, KEYBOARD_LEFT_SHIFT, KEY_BACKSPACE,
+    /* Layer 0, row 3, right side. */
+    KEYBOARD_RIGHT_CTRL, KEY_SPACE, PRE_FUNCTION(1), KEY_MINUS, KEY_QUOTE, KEY_ENTER
+};
 
 int layer1[44] = {
-    SHIFT(KEY_1), SHIFT(KEY_2), SHIFT(KEY_LEFT_BRACE), SHIFT(KEY_RIGHT_BRACE), SHIFT(KEY_BACKSLASH), KEY_0, KEY_PAGE_UP, KEY_7, KEY_8, KEY_9, SHIFT(KEY_8), 
-    SHIFT(KEY_3), SHIFT(KEY_4), SHIFT(KEY_9), SHIFT(KEY_0), KEY_TILDE, KEY_0, KEY_PAGE_DOWN, KEY_4, KEY_5, KEY_6, SHIFT(KEY_EQUAL), 
-    SHIFT(KEY_5), SHIFT(KEY_6), KEY_LEFT_BRACE, KEY_RIGHT_BRACE, SHIFT(KEY_TILDE), KEYBOARD_LEFT_ALT, SHIFT(KEY_7), KEY_1, KEY_2, KEY_3, KEY_BACKSLASH, 
-    FUNCTION(2), SHIFT(KEY_INSERT), KEYBOARD_LEFT_GUI, KEYBOARD_LEFT_SHIFT, KEY_BACKSPACE, KEYBOARD_RIGHT_CTRL, KEY_SPACE, PRE_FUNCTION(1), KEY_PERIOD, KEY_0, KEY_EQUAL };
+    /* Layer 1, row 0, left side. */
+    SHIFT(KEY_1), SHIFT(KEY_2), SHIFT(KEY_LEFT_BRACE), SHIFT(KEY_RIGHT_BRACE), SHIFT(KEY_BACKSLASH), KEY_0,
+    /* Layer 1, row 0, right side. */
+    KEY_PAGE_UP, KEY_7, KEY_8, KEY_9, SHIFT(KEY_8), 
+
+    /* Layer 1, row 1, left side. */
+    SHIFT(KEY_3), SHIFT(KEY_4), SHIFT(KEY_9), SHIFT(KEY_0), KEY_TILDE, KEY_0,
+    /* Layer 1, row 1, right side. */
+    KEY_PAGE_DOWN, KEY_4, KEY_5, KEY_6, SHIFT(KEY_EQUAL), 
+
+    /* Layer 1, row 2, left side. */
+    SHIFT(KEY_5), SHIFT(KEY_6), KEY_LEFT_BRACE, KEY_RIGHT_BRACE, SHIFT(KEY_TILDE), KEYBOARD_LEFT_ALT,
+    /* Layer 1, row 2, right side. */
+    SHIFT(KEY_7), KEY_1, KEY_2, KEY_3, KEY_BACKSLASH, 
+
+    /* Layer 1, row 3, left side. */
+    FUNCTION(2), SHIFT(KEY_INSERT), KEYBOARD_LEFT_GUI, KEYBOARD_LEFT_SHIFT, KEY_BACKSPACE, KEYBOARD_RIGHT_CTRL,
+    /* Layer 1, row 3, right side. */
+    KEY_SPACE, PRE_FUNCTION(1), KEY_PERIOD, KEY_0, KEY_EQUAL
+};
 
 #include "layer2.h"
 
